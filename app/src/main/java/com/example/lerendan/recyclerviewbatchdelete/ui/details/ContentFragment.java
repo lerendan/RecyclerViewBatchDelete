19e629fc3e2464b8e5c7ab397556db6f02d87f45
Lerendan <340212100@qq.com>
2016-08-03 16:28:37 +0800

    capter 12

---
+++ app/src/main/java/com/example/lerendan/recyclerviewbatchdelete/ui/details/ContentFragment.java
@@ -0,0 +1,91 @@
+package com.example.lerendan.recyclerviewbatchdelete.ui.details;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+
+import com.example.lerendan.recyclerviewbatchdelete.R;
+import com.example.lerendan.recyclerviewbatchdelete.bean.Crime;
+import com.example.lerendan.recyclerviewbatchdelete.bean.CrimeLab;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Administrator on 2016/8/3.
+ */
+
+public class ContentFragment extends Fragment{
+
+    private static final String ARG_CRIME_ID = "crime_id";
+    private static final String DIALOG_DATE = "DialogData";
+    private static final int REQUEST_DATE = 0;
+
+    private EditText edt_title;
+    private Button btn_data;
+    private CheckBox cb_solved;
+    private Crime mCrime;
+
+
+    public static ContentFragment newInstance(UUID crimeID){
+        Bundle args = new Bundle();
+        args.putSerializable(ARG_CRIME_ID,crimeID);
+        ContentFragment fragment = new ContentFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.content_fragment,container,false);
+
+        edt_title = (EditText) view.findViewById(R.id.title);
+        btn_data = (Button) view.findViewById(R.id.data);
+        cb_solved = (CheckBox) view.findViewById(R.id.solved);
+
+        UUID crimeID = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
+        mCrime = CrimeLab.get(getActivity()).getCrime(crimeID);
+
+        edt_title.setText(mCrime.getTitle());
+        updateDate();
+        cb_solved.setChecked(mCrime.isSolved());
+
+        btn_data.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager manager = getFragmentManager();
+                DataPickerFragment dialog = DataPickerFragment.newInstance(mCrime.getData());
+                dialog.setTargetFragment(ContentFragment.this,REQUEST_DATE);
+                dialog.show(manager,DIALOG_DATE);
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode!= Activity.RESULT_OK){
+            return;
+        }
+
+        if(requestCode==REQUEST_DATE){
+            Date date = (Date) data.getSerializableExtra(DataPickerFragment.EXTRA_DATE);
+            mCrime.setData(date);
+            updateDate();
+
+        }
+    }
+
+    private void updateDate() {
+        btn_data.setText(mCrime.getData().toString());
+    }
+}
