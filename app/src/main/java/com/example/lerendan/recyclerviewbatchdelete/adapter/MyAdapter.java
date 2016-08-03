7435913529a137c845632f834d06f65fe9258ba8
Lerendan <340212100@qq.com>
2016-08-03 16:22:26 +0800

    capter 12

--- app/src/main/java/com/example/lerendan/recyclerviewbatchdelete/adapter/MyAdapter.java
+++ app/src/main/java/com/example/lerendan/recyclerviewbatchdelete/adapter/MyAdapter.java
@@ -1,17 +1,18 @@
 package com.example.lerendan.recyclerviewbatchdelete.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.lerendan.recyclerviewbatchdelete.R;
-import com.example.lerendan.recyclerviewbatchdelete.bean.MyBean;
+import com.example.lerendan.recyclerviewbatchdelete.bean.Crime;
+import com.example.lerendan.recyclerviewbatchdelete.ui.frame.ContentPagerActivity;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
@@ -23,9 +24,9 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     private Context mContext;
     private LayoutInflater inflater;
-    private List<MyBean> mData;
+    private List<Crime> mData;
 
-    public MyAdapter(Context context, List<MyBean> data){
+    public MyAdapter(Context context, List<Crime> data){
         mContext  = context;
         inflater =  LayoutInflater.from(context);
         mData = data;
@@ -39,7 +40,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
-        holder.textView.setText(mData.get(position).getTitle());
+        Crime crime=mData.get(position);
+        holder.bindCrime(crime);
 
     }
 
@@ -49,16 +51,31 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        TextView textView;
+        private TextView mTitleTextView;
+        private TextView mDataTextView;
+        private CheckBox mCheckBox;
+        private Crime mCrime;
         public MyViewHolder(View itemView) {
             super(itemView);
             itemView.setOnClickListener(this);
-            textView = (TextView) itemView.findViewById(R.id.textview);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.title);
+            mDataTextView = (TextView) itemView.findViewById(R.id.data);
+            mCheckBox = (CheckBox) itemView.findViewById(R.id.mCheckBox);
         }
+
+        public void bindCrime(Crime crime){
+            mCrime =crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDataTextView.setText(mCrime.getData().toString());
+            mCheckBox.setChecked(mCrime.isSolved());
+        }
+
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(mContext, "ggg", Toast.LENGTH_SHORT).show();
+            Intent intent = ContentPagerActivity.newIntent(mContext,mCrime.getId());
+            mContext.startActivity(intent);
+//            Toast.makeText(mContext, mCrime.getTitle(), Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/com/example/lerendan/recyclerviewbatchdelete/ui/frame/MainActivity.java
+++ app/src/main/java/com/example/lerendan/recyclerviewbatchdelete/ui/frame/MainActivity.java
@@ -1,52 +1,31 @@
 package com.example.lerendan.recyclerviewbatchdelete.ui.frame;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 
 import com.example.lerendan.recyclerviewbatchdelete.R;
-import com.example.lerendan.recyclerviewbatchdelete.adapter.MyAdapter;
-import com.example.lerendan.recyclerviewbatchdelete.bean.MyBean;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.example.lerendan.recyclerviewbatchdelete.ui.details.ListFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private MyAdapter myAdapter;
-    private List<MyBean> mData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initData();
-        initView();
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.container);
 
-        setListener();
-    }
-
-    private void initData() {
-        mData = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            MyBean myBean = new MyBean("title"+i);
-            mData.add(myBean);
+        if (fragment == null) {
+            fragment = new ListFragment();
+            fm.beginTransaction().add(R.id.container, fragment).commit();
         }
+
     }
 
-    private void initView() {
-        mRecyclerView = (RecyclerView) findViewById(R.id.mRecyclerView);
-        myAdapter = new MyAdapter(MainActivity.this,mData);
-        mRecyclerView.setLayoutManager(new GridLayoutManager(this,2));
-        mRecyclerView.setAdapter(myAdapter);
-    }
-
-    private void setListener() {
-    }
 
 
 }
